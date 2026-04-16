8211512e86ae5a6cd7c86e22f942dbdd3ca369cb
Sierazeke <kivanovs@Opti-08.localdomain>
2026-04-16 10:33:09 +0300

    First Commit

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dev Store</title>
+</head>
+<style>
+    * {
+        font-family: monospace;
+    }
+</style>
+<body>
+    <h1>Store!</h1>
+</body>
+</html>
